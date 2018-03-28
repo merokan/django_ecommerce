7909b1b01533b01c50510cd9c638bd774a57d844
Sargon Saadi <sargonsaadi@gmail.com>
2018-03-27 18:17:06 -0700

    working loginReg and updated Home.html

--- apps/drinksrus/views.py
+++ apps/drinksrus/views.py
@@ -4,13 +4,9 @@ from models import *
 import bcrypt
 
 def index(request):
-    if 'id' in request.session:
-        return redirect('/home')
-    else:
-        context = {
-            "user" : User.objects.all()
-        }
-        return render(request, 'drinksrus/index.html', context)
+    request.session.clear()
+    
+    return render(request, 'drinksrus/index.html')
 
 def register(request):
     errors = User.objects.register(request.POST)
@@ -29,10 +25,10 @@ def login(request):
         if len(errors) >0:
             return redirect('/')
         else:
-            user= User.objects.get(email =request.Post['email'])
+            user= User.objects.get(email =request.POST['email'])
             request.session['id'] = user.id
             request.session['cart'] = {}
-            return redirect('/home')
+            return redirect('/home/'+str(1))
     else:
         return redirect('/')
 
@@ -42,11 +38,21 @@ def home(request, pagenum):
     else:
         context = {
             "categories": Category.objects.all(),
-            "products": Product.objects.all().order_by("-created_at") [((pagenum-1) * 15 ) :((pagenum-1) * 15 ) +15],
-            "pages": pagenum,
+            "products": Product.objects.all().order_by("-created_at") [ (((int(pagenum)-1)) * 15 )  :(((int(pagenum)-1)) * 15 ) +15],
+            "pagenum": pagenum,
             "cart": request.session['cart']
         }
         return render(request, 'drinksrus/home.html', context)
+def search (request):
+    if request.method =="POST":
+        context ={
+            "products": Product.objects.filter(item__contains = request.POST['searchName']),
+            "categories": Category.objects.all(),
+            "cart": request.session['cart']
+        }
+        return render(request, 'drinksrus/home.html', context)
+    else:
+        return redirect('/')
 
 def logout(request):
     del request.session['id']
