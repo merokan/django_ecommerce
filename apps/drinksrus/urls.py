453800b5a246f3c53f678e36e56897d5e5f67ad3
Sargon Saadi <sargonsaadi@gmail.com>
2018-03-28 13:22:56 -0700

    admin validations & urls/views

--- apps/drinksrus/urls.py
+++ apps/drinksrus/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^register$', views.register),
     url(r'^login$', views.login),
+    url(r'^admin_login$', views.admin_login),
     url(r'^home/(?P<pagenum>\d+)$', views.home),
+    url(r'^product/(?P<prodnum>\d+)$', views.home),
     url(r'^logout$', views.logout),
-    url(r'^search$',views.search)
+    url(r'^search$',views.search),
+    url(r'^admin_dashboard$',views.admindashboard),
 ]
